c67a94da66bf904e246ed04f3bc978456b29d2df
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-02-24 23:59:01 +0000

    print email and username

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,45 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+
 export default function page() {
-  return (
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    // Handle loading state however you like
+    return null;
+  }
+
+  if (isSignedIn && user && user.fullName) {
+    return (
+      <div className=" pl-7 pt-3">
+        <h1 className="text-[23px] font-bold">Hello {user.fullName}!</h1>
+        <div>
+          {user.emailAddresses.map((email) => (
+            <div key={email.emailAddress}>{email.emailAddress}, </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return <div>Not signed in</div>;
+}
+
+{
+  /**
+ return (
     <>
       <div className="flex gap-4 pl-7 pt-3">
         <h1 className="text-[23px] font-bold">Heureux de vous revoir</h1>
       </div>
     </>
   );
+
+   const { isSignedIn } = useAuth();
+  const { isLoaded } = useSignIn();
+  const { user } = useUser;
+
+   useAuth, useSignIn,
+ */
 }
--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -1,11 +1,22 @@
-"use client";
-
-import { UserProfile } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 export default function page() {
-  return (
-    <>
-      <UserProfile />
-    </>
-  );
+  const { isSignedIn, user, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    // Handle loading state however you like
+    return null;
+  }
+
+  if (isSignedIn) {
+    return <div>Hello {user.fullName}!</div>;
+  }
+
+  return <div>Not signed in</div>;
+}
+
+{
+  /**
+   *
+   */
 }
