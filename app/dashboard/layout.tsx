d5a8792bc223887f29f1a4f80e2629c9998ff3fa
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-03-04 21:11:58 +0000

    build responsive sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,50 @@
+"use client";
+
 import Sidebar from "@/app/dashboard/_components/Sidebar";
+import { Menu, X } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [sidebarIcon, setSidebarIcon] = useState(<Menu size={24} />);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+    setSidebarIcon(isSidebarOpen ? <Menu size={24} /> : <X size={24} />);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSidebarOpen(window.innerWidth >= 1024);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="flex items-start justify-between">
-      <div className="hidden lg:flex min-w-[300px] border-r min-h-screen">
+    <div className=" flex items-start justify-between">
+      <div
+        className={` ${
+          isSidebarOpen ? "min-w-[300px]" : "hidden"
+        } border-r min-h-screen`}
+      >
         <Sidebar />
       </div>
-      <div className="w-full h-full ">{children}</div>
+
+      <div className="relative w-full h-full">
+        {window.innerWidth < 1024 && (
+          <button className="absolute top-6 left-4" onClick={toggleSidebar}>
+            {sidebarIcon}
+          </button>
+        )}
+        {children}
+      </div>
     </div>
   );
 }
